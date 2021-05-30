62574af2f6bb0ad366662d6c22fb3d73d97cebb6
Max <m412tinez@gmail.com>
2021-05-30 00:12:49 -0500

    added click listener and adjusting function to save txt to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 var currentDate= moment().format('dddd, MMM Do YYYY');
     $('#currentDay').html(currentDate);
+
+$(document).ready(function() {
+    console.log("ready!");
+    $('.saveBtn').on('click', function() {
+        var textInput= $(this).siblings('.description').val();
+        localStorage.setItem(textInput);
+    })
+    });
