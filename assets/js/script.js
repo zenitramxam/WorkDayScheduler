564bd5e4195b2e6da69ddf76b64b4424bae40ce4
Max <m412tinez@gmail.com>
2021-06-01 22:08:34 -0500

    completed all textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,65 @@ var currentDate= moment().format('dddd, MMM Do YYYY,');
 $(document).ready(function() {
     console.log("ready!");
     $('.saveBtn1').on('click', function() {
-        //var textInput= document.querySelector('#8text');
         var textInput1= document.querySelector('#descript1');
         var time= $(this).parent().attr('id');
-        //localStorage.setItem('textInput', JSON.stringify(textInput));
-        //textInput.value = localStorage.getItem('comment')
         localStorage.setItem(time, textInput1.value);
-        console.log(localStorage);
     })
     });
-$('#8oclock .description').val(localStorage.getItem('8oclock'));
+$('#9oclock .description').val(localStorage.getItem('9oclock'));
 
-$(document).ready(function() {
-    console.log("ready!");
     $('.saveBtn2').on('click', function() {
         var textInput2= document.querySelector('#descript2');
         var time= $(this).parent().attr('id');
         localStorage.setItem(time, textInput2.value);
-        console.log(localStorage);
-    })
     });
-$('#9oclock .description').val(localStorage.getItem('9oclock'));
+$('#10oclock .description').val(localStorage.getItem('10oclock'));
 
     $('.saveBtn3').on('click', function() {
         var textInput3= document.querySelector('#descript3');
         var time= $(this).parent().attr('id');
         localStorage.setItem(time, textInput3.value);
-        console.log(localStorage);
     });
-$('#10oclock .description').val(localStorage.getItem('10oclock'));
+$('#11oclock .description').val(localStorage.getItem('11oclock'));
 
+$('.saveBtn4').on('click', function() {
+    var textInput4= document.querySelector('#descript4');
+    var time= $(this).parent().attr('id');
+    localStorage.setItem(time, textInput4.value);
+});
+$('#12oclock .description').val(localStorage.getItem('12oclock'));
+
+$('.saveBtn5').on('click', function() {
+    var textInput5= document.querySelector('#descript5');
+    var time= $(this).parent().attr('id');
+    localStorage.setItem(time, textInput5.value);
+});
+$('#1oclock .description').val(localStorage.getItem('1oclock'));
+
+$('.saveBtn6').on('click', function() {
+    var textInput5= document.querySelector('#descript6');
+    var time= $(this).parent().attr('id');
+    localStorage.setItem(time, textInput5.value);
+});
+$('#2oclock .description').val(localStorage.getItem('2oclock'));
+
+$('.saveBtn7').on('click', function() {
+    var textInput5= document.querySelector('#descript7');
+    var time= $(this).parent().attr('id');
+    localStorage.setItem(time, textInput5.value);
+});
+$('#3oclock .description').val(localStorage.getItem('3oclock'));
+
+$('.saveBtn8').on('click', function() {
+    var textInput5= document.querySelector('#descript8');
+    var time= $(this).parent().attr('id');
+    localStorage.setItem(time, textInput5.value);
+});
+$('#4oclock .description').val(localStorage.getItem('4oclock'));
+
+$('.saveBtn9').on('click', function() {
+    var textInput5= document.querySelector('#descript9');
+    var time= $(this).parent().attr('id');
+    localStorage.setItem(time, textInput5.value);
+});
+$('#5oclock .description').val(localStorage.getItem('5oclock'));
