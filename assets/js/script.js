4ebffba8fe0ce873dd18dd2469ba1639810cfa19
Max <m412tinez@gmail.com>
2021-06-01 19:55:38 -0500

    added function to save textarea to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
-var currentDate= moment().format('dddd, MMM Do YYYY');
+var currentDate= moment().format('dddd, MMM Do YYYY,');
     $('#currentDay').html(currentDate);
 
 $(document).ready(function() {
     console.log("ready!");
-    $('.saveBtn').on('click', function() {
-        var textInput= $(this).siblings('.description').val();
-        localStorage.setItem(textInput);
+    $('.saveBtn1').on('click', function() {
+        //var textInput= document.querySelector('#8text');
+        var textInput1= document.querySelector('#descript1');
+        var time= $(this).parent().attr('id');
+        //localStorage.setItem('textInput', JSON.stringify(textInput));
+        //textInput.value = localStorage.getItem('comment')
+        localStorage.setItem(time, textInput1.value);
+        console.log(localStorage);
     })
     });
+$('#8oclock .description').val(localStorage.getItem('8oclock'));
+
+$(document).ready(function() {
+    console.log("ready!");
+    $('.saveBtn2').on('click', function() {
+        var textInput2= document.querySelector('#descript2');
+        var time= $(this).parent().attr('id');
+        localStorage.setItem(time, textInput2.value);
+        console.log(localStorage);
+    })
+    });
+$('#9oclock .description').val(localStorage.getItem('9oclock'));
+
+    $('.saveBtn3').on('click', function() {
+        var textInput3= document.querySelector('#descript3');
+        var time= $(this).parent().attr('id');
+        localStorage.setItem(time, textInput3.value);
+        console.log(localStorage);
+    });
+$('#10oclock .description').val(localStorage.getItem('10oclock'));
+
